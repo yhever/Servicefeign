fd05d8e7de25db52544f9d9394d5cd23d2f97ad9
calvin <calvin.ye@capgemini.com>
2017-12-25 14:34:31 +0800

    update

--- ServiceFeign/src/main/java/com/yhever/servicefeign/ServiceFeignApplication.java
+++ ServiceFeign/src/main/java/com/yhever/servicefeign/ServiceFeignApplication.java
@@ -1,5 +1,10 @@
 package com.yhever.servicefeign;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 /**
  * Hello world!
  *
--- ServiceFeign/src/main/java/com/yhever/servicefeign/controller/WelcomeController.java
+++ ServiceFeign/src/main/java/com/yhever/servicefeign/controller/WelcomeController.java
@@ -1,5 +1,11 @@
 package com.yhever.servicefeign.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.yhever.servicefeign.service.WelcomeService;
 
 @RestController
